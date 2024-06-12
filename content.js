fec2616502dc9d0574e2d92dfe511d77db0c1643
lucasvavon <lucasvavon@icloud.com>
2024-06-12 18:18:17 +0200

    list videos in popup

--- content.js
+++ content.js
@@ -3,16 +3,16 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         const video = document.querySelector('video');
         if (video) {
             const videoData = {
-                currentTime: video.currentTime,
-                duration: video.duration,
-                paused: video.paused,
-                volume: video.volume,
-                playbackRate: video.playbackRate,
-                src: location.href
+                userId: 21,
+                title: 'test',
+                link: location.href,
+                moment: video.currentTime
             };
-            sendResponse(videoData);
+
+            sendResponse(videoData)
+
         } else {
-            sendResponse({ error: 'No video element found' });
+            sendResponse({error: 'No video element found'});
         }
     }
 });
--- popup.js
+++ popup.js
@@ -15,10 +15,44 @@ document.getElementById('extractButton').addEventListener('click', () => {
                     } else if (response && response.error) {
                         document.getElementById('result').textContent = response.error;
                     } else {
-                        document.getElementById('result').textContent = JSON.stringify(response, null, 2);
+
+                        fetch("http://localhost:8080/api/videos", {
+                            method: "post",
+                            headers: {
+                                'Accept': 'application/json',
+                                'Content-Type': 'application/json'
+                            },
+
+                            body: JSON.stringify(response)
+                        })
+                            .then((res) => {
+                                console.log(res);
+                            })
+                            .catch(err => {
+                                console.log(err);
+                            })
                     }
                 });
             }
         );
     });
 });
+document.addEventListener("DOMContentLoaded", async function () {
+    const videos = await getUserVideos(21)
+    videos.forEach(video => {
+        document.getElementById('result').innerHTML += videoCard(video.Title, video.Link, video.Moment);
+    })
+});
+
+function videoCard(title, link, moment) {
+    return `<a href="${link}&t=${moment}" class="video-card">
+                <img src="https://www.numerama.com/wp-content/uploads/2017/11/macos-high-sierra.jpg" alt="Video Thumbnail">
+                <div class="play-button">&#x25B6;</div>
+                <div class="title">${title}</div>
+              </a>`;
+}
+
+async function getUserVideos(id) {
+    const response = await fetch(`http://localhost:8080/api/videos/user/${id}`)
+    return await response.json();
+}
